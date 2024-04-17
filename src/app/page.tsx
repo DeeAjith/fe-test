9cdf3923085e65f5f0b0875f48c6756cbc05e820
DeeAjith <41795417+DeeAjith@users.noreply.github.com>
2024-04-18 02:08:40 +0530

    dummy page-content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
 export default function Home() {
   return (
     <main className="page-content flex">
-      <div className="max-w-screen-xl mx-auto">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab dolore
-        ipsa dolorum aut voluptatum quis nobis minima veniam cumque ducimus non
-        fuga, harum deleniti modi. Debitis porro tenetur, quae pariatur ab
-        magnam aliquam molestias velit minima assumenda quis ipsam amet facere
-        aperiam fugit dolore odio eaque cum? Nam porro enim iusto dolorem?
-        Iusto, ea labore esse dolore numquam sequi reiciendis sit nihil quaerat
-        minima sed omnis ducimus molestiae fugiat nulla aliquid assumenda
-        placeat facilis autem. Fugiat molestias fuga doloribus aut architecto
-        impedit ducimus numquam tenetur ipsam deleniti placeat consectetur harum
-        nulla optio nisi doloremque eum quasi, pariatur magni laboriosam facere
-        dolorem autem! Voluptatem, nemo soluta enim officiis ipsam, consequatur
-        excepturi fugiat accusantium optio vel labore inventore totam quibusdam?
-        Perferendis!
+      <div className="max-w-screen-xl mx-auto my-20">
+        <div className="flex overflow-x-auto gap-x-5 whitespace-nowrap mb-8">
+          <div className="flex items-center px-5 py-3 font-medium text-gray-500 rounded-lg cursor-pointer bg-gray-50 gap-x-2">
+            All category
+          </div>
+          <div className="flex items-center px-5 py-3 font-medium text-gray-900 bg-gray-100 rounded-lg cursor-pointer gap-x-2 hover:bg-gray-50 hover:text-gray-500">
+            Frontend Developer
+          </div>
+          <div className="flex items-center px-5 py-3 font-medium text-gray-900 bg-gray-100 rounded-lg cursor-pointer gap-x-2 hover:bg-gray-50 hover:text-gray-500">
+            Backend Developer
+          </div>
+          <div className="flex items-center px-5 py-3 font-medium text-gray-900 bg-gray-100 rounded-lg cursor-pointer gap-x-2 hover:bg-gray-50 hover:text-gray-500">
+            Fullstack Developer
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab
+          dolore ipsa dolorum aut voluptatum quis nobis minima veniam cumque
+          ducimus non fuga, harum deleniti modi. Debitis porro tenetur, quae
+          pariatur ab magnam aliquam molestias velit minima assumenda quis ipsam
+          amet facere aperiam fugit dolore odio eaque cum? Nam porro enim iusto
+          dolorem? Iusto, ea labore esse dolore numquam sequi reiciendis sit
+          nihil quaerat minima sed omnis ducimus molestiae fugiat nulla aliquid
+          assumenda placeat facilis autem. Fugiat molestias fuga doloribus aut
+          architecto impedit ducimus numquam tenetur ipsam deleniti placeat
+          consectetur harum nulla optio nisi doloremque eum quasi, pariatur
+          magni laboriosam facere dolorem autem! Voluptatem, nemo soluta enim
+          officiis ipsam, consequatur excepturi fugiat accusantium optio vel
+          labore inventore totam quibusdam? Perferendis!
+        </p>
       </div>
     </main>
   )
