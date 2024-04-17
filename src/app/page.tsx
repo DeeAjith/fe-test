05a3b7e9af7c1ee744b82430040e3e8e1ac5976a
DeeAjith <41795417+DeeAjith@users.noreply.github.com>
2024-04-18 02:05:24 +0530

    added some main page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 export default function Home() {
   return (
     <main className="page-content flex">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At maiores
-      commodi ut aliquam quidem, inventore beatae accusantium facere quos
-      explicabo?
+      <div className="max-w-screen-xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab dolore
+        ipsa dolorum aut voluptatum quis nobis minima veniam cumque ducimus non
+        fuga, harum deleniti modi. Debitis porro tenetur, quae pariatur ab
+        magnam aliquam molestias velit minima assumenda quis ipsam amet facere
+        aperiam fugit dolore odio eaque cum? Nam porro enim iusto dolorem?
+        Iusto, ea labore esse dolore numquam sequi reiciendis sit nihil quaerat
+        minima sed omnis ducimus molestiae fugiat nulla aliquid assumenda
+        placeat facilis autem. Fugiat molestias fuga doloribus aut architecto
+        impedit ducimus numquam tenetur ipsam deleniti placeat consectetur harum
+        nulla optio nisi doloremque eum quasi, pariatur magni laboriosam facere
+        dolorem autem! Voluptatem, nemo soluta enim officiis ipsam, consequatur
+        excepturi fugiat accusantium optio vel labore inventore totam quibusdam?
+        Perferendis!
+      </div>
     </main>
   )
 }
